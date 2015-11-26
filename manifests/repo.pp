4ef8f1e5ab796eda46fc93a591c9ed0b4864be52
Andy Newton <andy.newton@snowflakesoftware.com>
2015-11-26 14:09:04 +0000

    Make repo key work with latest apt module, tidy up lint

--- manifests/repo.pp
+++ manifests/repo.pp
@@ -1,21 +1,26 @@
 class hipchat::repo {
   
-  case $osfamily {
+  case $::osfamily {
     'Debian': {
       
       apt::source { 'hipchat':
-			  location          => 'http://downloads.hipchat.com/linux/apt',
-			  release           => 'stable',
-			  repos             => 'main',
-			  include_src       => false,
-			}
-			
-			apt::key { 'hipchat':
-			  key_source => 'https://www.hipchat.com/keys/hipchat-linux.key',
-			}
-			
+        location    => 'http://downloads.hipchat.com/linux/apt',
+        release     => 'stable',
+        repos       => 'main',
+        include_src => false,
+      }
+      
+      apt::key { 'hipchat':
+        id     => '69F57C04EA38EEE7A47E9BCCAAD4AA21729B5780',
+        source => 'https://www.hipchat.com/keys/hipchat-linux.key',
+      }
+
     }
-    
+
+    default: {
+      notify{"Hipchat repo: OS family ${::osfamily} not supported":}
+    }
+
   }
-  
+
 }
